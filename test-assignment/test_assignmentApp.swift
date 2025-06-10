e536bd26e0b6bf5b03723645c274a4c069bb045c
Vlad <vladyslav>
2025-06-10 23:17:25 +0300

    Initial Commit

---
+++ test-assignment/test_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_assignmentApp.swift
+//  test-assignment
+//
+//  Created by Vlad on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct test_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
